c50b2fb731f69959fb5a350fefbc77440805b404
Patrick Connolly <patrick.c.connolly@gmail.com>
2015-12-19 20:17:06 -0400

    Less scripts clutter.

---
+++ www/js/localnotifications.js
@@ -0,0 +1,15 @@
+scheduleDelayed = function () {
+    var now = new Date().getTime(),
+        _5_sec_from_now = new Date(now + 5 * 1000);
+
+    cordova.plugins.notification.local.schedule({
+        id: 1,
+        icon: 'res://icon',
+        // See: http://androiddrawables.com
+        // Most icons with 'ic' prefix work.
+        smallIcon: null,
+        title: '3 upcoming votes',
+        text: 'City Council',
+        at: _5_sec_from_now,
+    });
+};
