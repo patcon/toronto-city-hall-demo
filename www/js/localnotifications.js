84e458424b83691c5962d625680a19e04f72cbc9
Patrick Connolly <patrick.c.connolly@gmail.com>
2015-12-20 03:26:52 -0400

    Use data.json for demo.

--- www/js/localnotifications.js
+++ www/js/localnotifications.js
@@ -1,14 +1,11 @@
-scheduleDelayed = function () {
+scheduleDelayed = function (data) {
   var now = new Date().getTime(),
       _5_sec_from_now = new Date(now + 5 * 1000);
 
   cordova.plugins.notification.local.schedule({
       id: 1,
       icon: 'res://icon',
-      // See: http://androiddrawables.com
-      // Most icons with 'ic' prefix work.
-      smallIcon: null,
-      title: '3 upcoming votes',
+      title: data.upcoming.count + ' upcoming votes',
       text: 'City Council',
       at: _5_sec_from_now,
   });
@@ -16,6 +13,21 @@ scheduleDelayed = function () {
 
 document.addEventListener('deviceready', onDeviceReady, false);
 function onDeviceReady() {
+  var path = window.location.href.replace('index.html', '');
+  var request = pegasus(path + 'data.json');
+  request.then(
+    function (data, xhr) {
+      cordova.plugins.notification.local.on("click", function (notification) {
+        if (notification.id == 1) {
+          scheduleDelayed(data);
+        };
+      });
+    },
+    function (data, xhr) {
+      console.error(data, xhr.status);
+    }
+  );
+
   cordova.plugins.notification.local.schedule({
       id: 1,
       icon: 'res://icon',
@@ -24,11 +36,5 @@ function onDeviceReady() {
       text: 'Click to start demo',
       at: new Date(),
       ongoing: true,
-  });
-
-  cordova.plugins.notification.local.on("click", function (notification) {
-    if (notification.id == 1) {
-      scheduleDelayed();
-    };
   });
 };
