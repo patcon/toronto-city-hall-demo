d6bf3d0c03ac01e89931f9cbd56f781abdc924da
Patrick Connolly <patrick.c.connolly@gmail.com>
2015-12-20 02:19:09 -0400

    Better way to start demo.

--- www/js/localnotifications.js
+++ www/js/localnotifications.js
@@ -1,20 +1,34 @@
 scheduleDelayed = function () {
-    var now = new Date().getTime(),
-        _5_sec_from_now = new Date(now + 5 * 1000);
+  var now = new Date().getTime(),
+      _5_sec_from_now = new Date(now + 5 * 1000);
 
-    cordova.plugins.notification.local.schedule({
-        id: 1,
-        icon: 'res://icon',
-        // See: http://androiddrawables.com
-        // Most icons with 'ic' prefix work.
-        smallIcon: null,
-        title: '3 upcoming votes',
-        text: 'City Council',
-        at: _5_sec_from_now,
-    });
+  cordova.plugins.notification.local.schedule({
+      id: 1,
+      icon: 'res://icon',
+      // See: http://androiddrawables.com
+      // Most icons with 'ic' prefix work.
+      smallIcon: null,
+      title: '3 upcoming votes',
+      text: 'City Council',
+      at: _5_sec_from_now,
+  });
 };
 
 document.addEventListener('deviceready', onDeviceReady, false);
 function onDeviceReady() {
-  document.getElementById('btn-test-notification').addEventListener('click', scheduleDelayed);
-}
+  cordova.plugins.notification.local.schedule({
+      id: 1,
+      icon: 'res://icon',
+      // See: http://androiddrawables.com
+      // Most icons with 'ic' prefix work.
+      text: 'Click to start demo',
+      at: new Date(),
+      ongoing: true,
+  });
+
+  cordova.plugins.notification.local.on("click", function (notification) {
+    if (notification.id == 1) {
+      scheduleDelayed();
+    };
+  });
+};
